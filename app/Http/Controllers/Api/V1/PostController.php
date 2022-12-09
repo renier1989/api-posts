45f40f490abf44df629d493a2917e36916d809ff
Renier Vargas <rvargas@fenacrep.org>
2022-12-09 10:19:14 -0500

    Clase 10 | api - resolviendo el reto - Repaso

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -34,7 +34,14 @@ class PostController extends Controller
     {
         try {
             // $post->delete();
+            /** con esta linea puedo retornar la respuesta que estoy especificando en el mensaje */
             return response()->json(['message' => 'El Post fue elimindao con exito']);
+            
+            /** con esta linea puedo agregandole el estado de la respuesta 204, no muestro el mensaje pero estoy indicando
+             * que la operacion se ejecuto con exito
+             */
+            // return response()->json(['message' => 'El Post fue elimindao con exito'],204);
+            
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 422);        
         }
