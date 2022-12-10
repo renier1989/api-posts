4f8e689013dec40cbd950a6a04a680b411bc04e9
Renier Vargas <rvargas@fenacrep.org>
2022-12-10 18:23:23 -0500

    Clase 16 | api - Acceso con el token

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -2,24 +2,40 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function login(Request $request){
+        
         $this->validateLogin($request);
+        
+        if (Auth::attempt($request->only('email','password'))){
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ]);
+        }
 
-        // login true
+        return response()->json([
+            'message' => 'no esta autorizado para acceder'
+        ],401);
 
         // login false
     }
     
     public function validateLogin(Request $request){
-        return $request->validate([
-            'email' => 'required | email',
+        
+
+        $valid = $request->validate([
+            'email' => 'required|email',
             'password' => 'required',
             'name' => 'required',
         ]);
+        return $valid;
     }
 }
